e16f44e58741652c396fb4bf1fca5056abbdd96a
System Administrator <root@Harry-MacBook-Air.local>
2021-04-28 19:11:59 +1200

    store and carts functionality

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here.
 
@@ -8,7 +8,14 @@ class ProductAdmin(admin.ModelAdmin):
 	list_display_links = ('product_name', 'price', 'stock', 'category')
 	prepopulated_fields = {'slug': ('product_name',)}
 
+class VariationAdmin(admin.ModelAdmin):
+	list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+	list_editable = ('is_active',)
+	list_filter = ('product', 'variation_category', 'variation_value')
+
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
 
 
