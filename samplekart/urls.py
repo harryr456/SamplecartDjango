78bb02dcaf19a6fe1efa97a8efc381d06f7fa977
System Administrator <root@Harry-MacBook-Air.local>
2021-05-01 20:37:35 +1200

    Full Account Functionality

--- samplekart/urls.py
+++ samplekart/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
