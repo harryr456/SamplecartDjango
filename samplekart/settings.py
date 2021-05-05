a7384bbd33ff39848d480ac6de3c4ff08e8295ea
System Administrator <root@Harry-MacBook-Air.local>
2021-05-05 21:16:29 +1200

    Invoicing Functionality

--- samplekart/settings.py
+++ samplekart/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    'carts'
+    'carts',
+    'orders'
 ]
 
 MIDDLEWARE = [
@@ -146,7 +147,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'emailproject24@gmail.com'
-EMAIL_HOST_PASSWORD = 'szwlaxuccgvexyfa'
+EMAIL_HOST_PASSWORD = 'June4578&'
 DEFAULT_EMAIL_FROM = 'emailproject24@gmail.com'
 EMAIL_USE_TLS = True
 
